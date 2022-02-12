b37ad1371ce91d7b313da0a8b45f8490b62ee598
yahsuanlinus <savannahwang75@gmail.com>
2022-02-12 10:11:30 -0500

    redirect

--- db.js
+++ db.js
@@ -21,11 +21,14 @@ const Bookmark = db.define('Bookmark', {
 const seedAndSync = async() => {
 	await db.sync({force: true})
 	await Bookmark.create({id: 1, bookmark: 'linkedin.com', category: 'jobs'})
-	await Bookmark.create({id: 2, bookmark: 'indeed.com', category: 'jobs'})
-	await Bookmark.create({id: 3, bookmark: 'msdn.com', category: 'code'})
-	await Bookmark.create({id: 4, bookmark: 'stackoverflow.com', category: 'code'})
-	await Bookmark.create({id: 5, bookmark: 'amazon.com', category: 'shopping'})
-	await Bookmark.create({id: 6, bookmark: 'nordstrom', category: 'shopping'})
+	await Bookmark.create({id: 2, bookmark: 'monster.com', category: 'jobs'})
+	await Bookmark.create({id: 3, bookmark: 'indeed.com', category: 'jobs'})
+	await Bookmark.create({id: 4, bookmark: 'msdn.com', category: 'code'})
+	await Bookmark.create({id: 5, bookmark: 'stackoverflow.com', category: 'code'})
+	await Bookmark.create({id: 6, bookmark: 'amazon.com', category: 'shopping'})
+	await Bookmark.create({id: 7, bookmark: 'nordstrom.com', category: 'shopping'})
+	await Bookmark.create({id: 8, bookmark: 'leetcode.com', category: 'code'})
+	await Bookmark.create({id: 9, bookmark: 'google.com', category: 'code'})
 }
 
 
